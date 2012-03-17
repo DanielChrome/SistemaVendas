f966fdb5c39d9c2afc8d84677482b779e1a43af9
Daniel Chrome <hackerdownload@gmail.com>
2012-03-17 11:58:36 -0300

    1ºCommit: Criação da Classe Cliente.java

---
+++ src/br/edu/fasa/vendas/domainModel/Cliente.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fasa.vendas.domainModel;
+
+/**
+ *
+ * @author Chrome
+ */
+public class Cliente {
+    private int id;
+    private String nome;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+
+}
