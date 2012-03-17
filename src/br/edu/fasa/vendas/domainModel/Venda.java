5d8c7b5ec8aed21588c3b3037b7069a50f7bf48a
Daniel Chrome <hackerdownload@gmail.com>
2012-03-17 12:12:17 -0300

    Classe Venda.java criada.

---
+++ src/br/edu/fasa/vendas/domainModel/Venda.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fasa.vendas.domainModel;
+
+/**
+ *
+ * @author Chrome
+ */
+public class Venda {
+    private int codVenda;
+    private String data;
+    private int codCliente;
+
+    public int getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(int codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public int getCodVenda() {
+        return codVenda;
+    }
+
+    public void setCodVenda(int codVenda) {
+        this.codVenda = codVenda;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+    
+}
