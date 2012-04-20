cb92cdb1bec35989800238cce9ee40bb4ffe1caa
Daniel Chrome <hackerdownload@gmail.com>
2012-04-20 19:49:03 -0300

    Modificando projeto para JPA

---
+++ src/br/edu/fasa/vendas/domainModel/ClienteRepository.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fasa.vendas.domainModel;
+
+/**
+ *
+ * @author Chrome
+ */
+public interface ClienteRepository extends Repository<Cliente>{
+    
+}
